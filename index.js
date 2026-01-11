6ee185bd86bc82d0d00a081041e18849e926edc3
brain lamb <bdlamb3220@yahoo.com>
2026-01-11 14:19:44 -0800

    added the ability to change the title of a blog. title now appears more like a required filed. Fixed the patch to require an input for the title

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ app.use(express.json({ limit: '1mb', type: 'application/json' }));
 app.use(express.urlencoded({extended:true}));
 app.use(express.static("public"));
 
+var original="";
+
 app.get("/",(req,res)=>{
+    original="";
     if(blogData.length<1){
         res.render("index.ejs");
     } else{
@@ -17,12 +20,13 @@ app.get("/",(req,res)=>{
 });
 
 app.get("/create",(req,res)=>{
+    console.log("in create");
     res.render("index.ejs",{window:'create'});
 });
 
 app.get("/update",(req,res)=>{
     const title=req.query.title;
-    const text=blogData[title];
+    original=title;
     res.render("index.ejs",{ window:'update',title:title, text:blogData[title].toString()});
 });
 
@@ -32,6 +36,11 @@ app.post("/",(req,res)=>{
 });
 
 app.patch("/",(req,res)=>{
+    console.log("in patch");
+    console.log("orginal: "+original);
+    if(original != req.body.title){
+        delete blogData[`${original}`];
+    }
     blogData[`${req.body.title}`]=req.body.text;
     res.render("index.ejs",{blogData:Object.keys(blogData)});
 });
